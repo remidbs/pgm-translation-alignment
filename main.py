88608fe3edbdff81ed3812f7abf88440f1725951
tlacombe <hoetre@hotmail.fr>
2016-12-29 19:27:51 +0100

    Correction d un truc dans main. Du coup le HMM ne marche pas du tout en fait...

--- main.py
+++ main.py
@@ -63,6 +63,6 @@ print("...done")
 f2eTer = np.argmax(hmm.proba_f_knowing_e, axis = 1)
 print("HMM Translations :")
 for i in range(len(corpus.french_words)):
-    print corpus.french_words[i], " --> ", corpus.english_words[f2e[i]]
+    print corpus.french_words[i], " --> ", corpus.english_words[f2eTer[i]]
 
 # Just a bonus line for the PLAISIR
